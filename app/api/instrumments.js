94724f1ebf0491c65690ddc5aecba61b03649f97
raphaelconstantino <raphaelc23@gmail.com>
2016-12-26 20:02:37 -0200

    Adding env variable to mongo

--- app/api/instrumments.js
+++ app/api/instrumments.js
@@ -64,6 +64,7 @@ module.exports = function(app) {
 		})
 	};
 
+	console.log(app);
 	return api;
 };
 
--- app/api/songs.js
+++ app/api/songs.js
@@ -63,6 +63,7 @@ module.exports = function(app) {
 		})
 	};
 
+	console.log(app);
 	return api;
 };
 
