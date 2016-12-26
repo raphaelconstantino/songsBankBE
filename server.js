3c9de6fffc92dc0294a4fcd9395e179588318f7e
raphaelconstantino <raphaelc23@gmail.com>
2016-12-26 08:33:46 -0200

    Adding env variable to mongo

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express');
-require('./config/database')(process.env.MONGODB_URI);
+require('./config/database')('mongodb://localhost/playBank');
 
 http.createServer(app)
 .listen(process.env.PORT || 8080, function() {
